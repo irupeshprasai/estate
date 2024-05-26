0a26c1ede41948754d736ccb4000a8fb987bbc28
Rupesh <irupesh.prasai@gmail.com>
2024-05-26 15:17:37 +0545

    organized the calling apis

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,7 @@
 import express from 'express';
+import { test } from '../controllers/user.cotroller.js';
 
 const router = express.Router();
-router.get('/test',(req,res) => {
-    res.json({
-        message:'Hello World'
-    })
-})
+router.get('/test',test)
 
 export default router;
